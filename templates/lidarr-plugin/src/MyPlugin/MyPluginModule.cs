e39865e5fdc854e2a620add4dfab858f5e808297
RicherTunes <xfear26@hotmail.com>
2025-10-11 16:31:22 -0400

    template: add ICachePolicyProvider defaults + OAuth2 settings stub; update how-to

--- templates/lidarr-plugin/src/MyPlugin/MyPluginModule.cs
+++ templates/lidarr-plugin/src/MyPlugin/MyPluginModule.cs
@@ -1,4 +1,7 @@
 using Lidarr.Plugin.Common.Services.Http;
+using Lidarr.Plugin.Common.Services.Caching;
+using Lidarr.Plugin.Common.Interfaces;
+using MyPlugin.Policies;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MyPlugin;
@@ -11,8 +14,10 @@ public static class MyPluginModule
         services.AddHttpClient("myplugin")
             .ConfigureHttpClient(c => c.BaseAddress = new Uri(settings.BaseUrl));
 
+        // Cache policy provider (used by StreamingResponseCache implementations)
+        services.AddSingleton<ICachePolicyProvider, PolicyProvider>();
+
         // Example Resilience profile could be injected; using defaults for brevity
         return services;
     }
 }
-
